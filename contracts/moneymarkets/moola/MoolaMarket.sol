5149582f2167a3f25862cd6953b50dca08522f74
IntegralTeamR <repo@sfxdx.ru>
2022-05-23 11:45:29 +0700

    Integrate moola

---
+++ contracts/moneymarkets/moola/MoolaMarket.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.4;
+
+import {SafeERC20} from "../../libs/SafeERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {
+    AddressUpgradeable
+} from "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import {MoneyMarket} from "../MoneyMarket.sol";
+import {ILendingPool} from "./imports/ILendingPool.sol";
+import {
+    ILendingPoolAddressesProvider
+} from "./imports/ILendingPoolAddressesProvider.sol";
+
+contract AaveMarket is MoneyMarket {
+    using SafeERC20 for ERC20;
+    using AddressUpgradeable for address;
+
+    uint16 internal constant REFERRALCODE = 20; // Aave referral program code
+
+    ILendingPoolAddressesProvider public provider; // Used for fetching the current address of LendingPool
+    ERC20 public override stablecoin;
+    ERC20 public aToken;
+    address public rewards;
+
+    function initialize(
+        address _provider,
+        address _aToken,
+        address _rewards,
+        address _rescuer,
+        address _stablecoin
+    ) external initializer {
+        __MoneyMarket_init(_rescuer);
+
+        // Verify input addresses
+        require(
+            _provider.isContract() &&
+                _aToken.isContract() &&
+                _rewards != address(0) &&
+                _stablecoin.isContract(),
+            "MoolaMarket: An input address is not a contract"
+        );
+
+        provider = ILendingPoolAddressesProvider(_provider);
+        stablecoin = ERC20(_stablecoin);
+        aToken = ERC20(_aToken);
+        rewards = _rewards;
+    }
+
+    function deposit(uint256 amount) external override onlyOwner {
+        require(amount > 0, "MoolaMarket: amount is 0");
+
+        ILendingPool lendingPool = ILendingPool(provider.getLendingPool());
+
+        // Transfer `amount` stablecoin from `msg.sender`
+        stablecoin.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Approve `amount` stablecoin to lendingPool
+        stablecoin.safeIncreaseAllowance(address(lendingPool), amount);
+
+        // Deposit `amount` stablecoin to lendingPool
+        lendingPool.deposit(
+            address(stablecoin),
+            amount,
+            address(this),
+            REFERRALCODE
+        );
+    }
+
+    function withdraw(uint256 amountInUnderlying)
+        external
+        override
+        onlyOwner
+        returns (uint256 actualAmountWithdrawn)
+    {
+        require(amountInUnderlying > 0, "MoolaMarket: amountInUnderlying is 0");
+
+        ILendingPool lendingPool = ILendingPool(provider.getLendingPool());
+
+        // Redeem `amountInUnderlying` aToken, since 1 aToken = 1 stablecoin
+        // Transfer `amountInUnderlying` stablecoin to `msg.sender`
+        lendingPool.withdraw(
+            address(stablecoin),
+            amountInUnderlying,
+            msg.sender
+        );
+
+        return amountInUnderlying;
+    }
+
+    function claimRewards() external override {}
+
+    /**
+        Param setters
+     */
+    function setRewards(address newValue) external override onlyOwner {}
+
+    /**
+        @dev IMPORTANT MUST READ
+        This function is for restricting unauthorized accounts from taking funds
+        and ensuring only tokens not used by the MoneyMarket can be rescued.
+        IF YOU DON'T GET IT RIGHT YOU WILL LOSE PEOPLE'S MONEY
+        MAKE SURE YOU DO ALL OF THE FOLLOWING
+        1) You MUST override it in a MoneyMarket implementation.
+        2) You MUST make `super._authorizeRescue(token, target);` the first line of your overriding function.
+        3) You MUST revert during a call to this function if a token used by the MoneyMarket is being rescued.
+        4) You SHOULD look at how existing MoneyMarkets do it as an example.
+     */
+    function _authorizeRescue(address token, address target)
+        internal
+        view
+        override
+    {
+        super._authorizeRescue(token, target);
+        require(token != address(aToken), "MoolaMarket: no steal");
+    }
+
+    function _totalValue(
+        uint256 /*currentIncomeIndex*/
+    ) internal view override returns (uint256) {
+        return aToken.balanceOf(address(this));
+    }
+
+    function _incomeIndex() internal view override returns (uint256 index) {
+        ILendingPool lendingPool = ILendingPool(provider.getLendingPool());
+        index = lendingPool.getReserveNormalizedIncome(address(stablecoin));
+        require(index > 0, "MoolaMarket: BAD_INDEX");
+    }
+
+    uint256[45] private __gap;
+}
