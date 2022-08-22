cbbe99857908626d3aa7897def57c6dbcd487c92
IntegralTeamR <repo@sfxdx.ru>
2022-08-22 19:21:17 +0700

    Add config for celo & ganache

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -56,6 +56,25 @@ const config: HardhatUserConfig = {
         mnemonic: secret.mnemonic
       }
     },
+    ganache: {
+      url: "http://127.0.0.1:8545"
+    },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/52752'/0'/0"
+      },
+      chainId: 44787
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/52752'/0'/0"
+      },
+      chainId: 42220
+    },
     hardhat: {
       /*forking: {
         url:
